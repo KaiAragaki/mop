7ccacdd2daf3be319022d3cd5e68b1f078e40cde
Kai Aragaki <kaiaragaki@proton.me>
2025-07-31 12:17:58 +0200

    refactor: remove generics from spectramax
    They've been moved to their own file - just forgot to commit this :(

--- R/spectramax.R
+++ R/spectramax.R
@@ -35,30 +35,11 @@ obj_print_footer.spectramax <- function(x, ...) {
       "\n")
 }
 
-
-#' Get or set an object's plate data
-#'
-#' @param x Object to extract plate data from
-#' @param ... Arguments to be passed to their respective methods
-#'
-#' @return A `gp` object
-#' @export
-plate_data <- function(x, ...) {
-  UseMethod("plate_data")
-}
-
 #' @rdname plate_data
 #' @export
 plate_data.spectramax <- function(x, ...) {
   stopifnot(x$data[[1]]$type == "Plate")
   x$data[[1]]$data
-}
-
-#' @param value Value to set the plate to.
-#' @rdname plate_data
-#' @export
-'plate_data<-' <- function(x, value) {
-  UseMethod("plate_data<-", x)
 }
 
 #' @rdname plate_data
